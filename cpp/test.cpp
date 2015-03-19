02e75c58f0687998dd415e5f869c3bdc6faed140
dkruck <daniel.kruck@gmx.net>
2015-03-19 14:34:51 +0100

    Removed DeleteStateAction and MoveStateAction

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 //-- Qt
 #include <QtWidgets>
+#include <QSignalSpy>
 
 //-- Core
 #include <core/Core.h>
@@ -32,25 +33,34 @@ void LineEditTest::testGui() {
   MergedMainWindow w;
   w.show();
   // Connect signals
-  connect( this, SIGNAL(signal_select_choose(bool) ), w.action_Choose,
-      SLOT( setChecked(bool) ) );
-  emit signal_select_choose( true );
+  //connect( this, SIGNAL(signal_select_choose(bool) ), w.action_Choose,
+  //    SLOT( setChecked(bool) ) );
+  //emit signal_select_choose( true );
+  QSignalSpy spy( w.action_Choose, SIGNAL(triggered() ) );
 
 
 
   //w.action_State->trigger();
-  QTest::qWait(1000);
+  QTest::qWait(3000);
   //w.action_New->trigger();
   //QTest::qWait(1000);
   //QTest::mouseMove( &w, QPointF() ); // Move to center?
-  QTest::mouseMove(w.centralwidget, w.centralwidget->geometry().center() );
-  QTest::qWait(1000);
+  //QTest::mouseMove(w.centralwidget, w.centralwidget->geometry().center() );
+  QAction * chooseAction = w.action_Choose;
+  auto choose_action_rect = w.menu_Objects->actionGeometry( chooseAction );
+  QTest::mouseMove( &w, choose_action_rect.center() );
+  QTest::mousePress( &w, Qt::LeftButton, 0, choose_action_rect.center() );
+  QTest::mouseRelease( &w, Qt::LeftButton, 0, choose_action_rect.center() );
+  QTest::qWait( 10 );
+  std::cout << "spy.count << " << std::endl;
+  std::cout << spy.count() << std::endl ;
+  QTest::qWait(2000);
   //QTest::mousePress(&w, Qt::LeftButton, 0, w.centralwidget->geometry().center() );
-  QTest::qWait(10);
+  //QTest::qWait(10);
   //QTest::mouseRelease(&w, Qt::LeftButton, 0, w.centralwidget->geometry().center() );
-  QTest::qWait(1000);
+  //QTest::qWait(1000);
   //QTest::mouseMove( &w, w.action_ZoomIn->geometry().center() );
-  QTest::keyClicks( &w, QString("Hallo"), Qt::NoModifier, 1304);
+  //QTest::keyClicks( &w, QString("Hallo"), Qt::NoModifier, 1304);
   //QTest::mouseMoveEvent(& 
 }
 
